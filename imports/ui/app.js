e7a05db03ea13390d97ebceecff5968f8b23ed7d
Papi Diagne <abdiagne96@gmail.com>
2021-02-16 23:02:30 +0000

    started refactoring

--- imports/ui/app.js
+++ imports/ui/app.js
@@ -1,46 +1,10 @@
 import React from 'react';
+import MainMenu from './MainMenu'
+import AddForm from './AddForm'
 
 const App = () => (
   <>
-    <div className="app-mainmenu">
-      <div className="container">
-        <div className="menu-list">
-          <a className="list-item active" href="#">
-            <i className="item-icon icon-check" />
-            <p>Tasks</p>
-          </a>
-          <a className="list-item" href="#">
-            <i className="item-icon icon-toolkit" />
-            <p>Projects</p>
-          </a>
-          <a className="list-item" href="#">
-            <i className="item-icon icon-user" />
-            <p>People</p>
-          </a>
-          <a className="list-item" href="#">
-            <i className="item-icon icon-calendar" />
-            <p>Calendar</p>
-          </a>
-          <a className="list-item" href="#">
-            <i className="item-icon icon-inbox" />
-            <p>Inbox</p>
-          </a>
-          <a className="list-item" href="#">
-            <i className="item-icon icon-timer" />
-            <p>Timesheets</p>
-          </a>
-          <hr />
-          <a className="list-item" href="#">
-            <i className="item-icon icon-settings" />
-            <p>Settings</p>
-          </a>
-          <a className="list-item" onClick={() => console.log('support')}>
-            <i className="item-icon icon-question-mark" />
-            <p>Support</p>
-          </a>
-        </div>
-      </div>
-    </div>
+    <MainMenu />
     <div className="page">
       <div className="page-header">
         <div className="content has-back-button">
@@ -57,28 +21,7 @@ const App = () => (
         </div>
       </div>
       <div className="main-tasks-page content list-view">
-        <form className="add-task" noValidate="">
-          <div>
-            <div className="fieldset add-task-input fieldset-stripped">
-              <div className="fieldset-content">
-                <label className="fieldset-label">
-                  <span className="fieldset-label-content has-icon">
-                    <i className="icon-plus" />
-                  </span>
-                  <input
-                    className=""
-                    name="title"
-                    placeholder="Add new task"
-                    type="text"
-                    autoComplete="off"
-                    value=""
-                    onChange={() => {}}
-                  />
-                </label>
-              </div>
-            </div>
-          </div>
-        </form>
+        <AddForm />
         <div className="groups-wrapper">
           <div className="group-container open">
             <div className="group-head">
