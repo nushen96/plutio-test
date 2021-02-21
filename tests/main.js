d97abdcba453a8cb0ce9ff53e479ce9effc531a2
Papi Diagne <abdiagne96@gmail.com>
2021-02-21 22:46:40 +0000

    added testing

--- tests/main.js
+++ tests/main.js
@@ -1,7 +1,26 @@
-import { expect } from 'chai';
+import {assert} from 'chai';
+import {Tasks} from '/imports/api/tasks'
+import {create} from '../imports/api/tasks/methods/create';
+import {get} from '../imports/api/tasks/methods/get';
 
-describe('test', function() {
-  it('should pass', () => {
-    expect('plutio').to.eq('plutio');
+describe('Testing create() method', function() {
+  beforeEach(() => Tasks.remove({title: "testTask"}))
+  it('Should create a task', () => {
+    create({title: "testTask"})
+    const retrievedTask = Tasks.find({title: "testTask"}).fetch()
+    assert.isArray(retrievedTask, 'Mongo find method did not return an array')
+    assert.isNotEmpty(retrievedTask, "Retrieved array is empty")
+    assert.include(retrievedTask[0], {title: "testTask"})
   });
 });
+
+describe('Testing get() method', function() {
+  it('Should retrieve a list of tasks', () => {
+    const fetchedTasks = get()
+    assert.isArray(fetchedTasks, 'Mongo find method did not return an array')
+    if(fetchedTasks.length>0) {
+      assert.property(fetchedTasks[0], "title", "Fetched array does not contain tasks")
+    }
+  });
+});
+
